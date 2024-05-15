4ca9f629490cbbbb8f9782a5c7dbdb9f5b09697c
abruil-lecturer-vc <abruil@varsitycollege.co.za>
2024-05-15 22:22:34 +0200

    test

--- test/test/EmployeeTest.java
+++ test/test/EmployeeTest.java
@@ -2,8 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
  */
-package employeeclass;
+package test;
 
+import employeeclass.Employee;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
